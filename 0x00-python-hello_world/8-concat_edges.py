e1d356e5821986feff970e26378b1dd1f687cc58
suruur <surrarweesh@gmail.com>
2023-11-27 14:36:38 +0000

    all

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-
+str = (str.split(',')[2])[1:28] + " with Python"
 print(str)
