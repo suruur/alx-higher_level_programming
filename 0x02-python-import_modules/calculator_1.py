163da5d6cb66fc59f175e757ce93f05a7e9dad56
suruur <surrarweesh@gmail.com>
2023-11-30 10:32:01 +0000

    d

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+def add(a, b):
+    """my addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """my subtraction function
+
+    Args:
+        a: f
+        b: 
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My muliplication function
+
+    Args:
+        a: f 
+        b: s
+
+    Returns:
+        The return value. a * b;
+    """
+    return (a * b)
+
+def div(a, b):
+    """My divion function
+
+    Args:
+        a: f
+        b: s
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
