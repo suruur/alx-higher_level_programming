d6505a5ffe78ae470b34b5dfdd4a661175e8e4d1
suruur <surrarweesh@gmail.com>
2023-11-30 13:54:25 +0000

    FGDF

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,5 @@
 def magic_calculation(a, b):
-    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
+    add, sub = __import__('magic_calculation_102', globals(), locals(), ('add', 'sub'), 0)
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
