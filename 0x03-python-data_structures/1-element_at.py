ec1f5f27a1fcd53674c32ef071ae77dd4a875ade
suruur <surrarweesh@gmail.com>
2023-12-02 13:44:54 +0000

    d

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+
+    if __name__ == "__main__":
+        if idx < 0:
+            return ("None")
+        elif idx > len(my_list):
+            return ("None")
+        else:
+            for i in my_list:
+                return (my_list[idx])
