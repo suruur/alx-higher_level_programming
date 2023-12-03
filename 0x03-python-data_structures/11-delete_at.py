2f850a230c4cca2df7ead2ba604c0ae6e3508250
suruur <surrarweesh@gmail.com>
2023-12-03 11:52:40 +0000

    Del

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,9 +4,6 @@ def delete_at(my_list=[], idx=0):
     idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_l = []
-    for i, item in enumerate(my_list):
-        if i != idx:
-            new_l.append(item)
+    new_l = my_list[:idx] + my_list[idx + 1:]
 
     return new_l
