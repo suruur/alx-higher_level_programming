6d952bff8d14dbdae4fdfde1f95b235336def7fb
suruur <surrarweesh@gmail.com>
2023-12-03 11:21:28 +0000

    DIVIDED BY 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def devisible_by_2(my_list=[]):
 
-    if __name__ == "__main__":
-        res = []
-        for num in my_list:
-            res.append(num % 2 == 0)
+    res = []
+    for num in my_list:
+        res.append(num % 2 == 0)
 
-        return res
+    return res
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
 
-    if __name__ == "__main__":
-        if not my_list:
-            return None
+    if not my_list:
+        return None
 
-        max_v = my_list[0]
-        for num in my_list:
-            if num > max_v:
-                max_v = num
+    max_v = my_list[0]
+    for num in my_list:
+        if num > max_v:
+            max_v = num
 
-        return max_v
+    return max_v
