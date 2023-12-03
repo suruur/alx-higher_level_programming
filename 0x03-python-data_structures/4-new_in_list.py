a7db58b8f37d52ef1857b645cc1da213bbd224d6
suruur <surrarweesh@gmail.com>
2023-12-03 10:04:47 +0000

    AQWE

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
-    copied_list = my_list
+    if len(my_list) == 0:
+        copied_list = []
+        copied_list.append(element)
+    else:
+        copied_list = my_list
+
     if idx < 0:
         return (copied_list)
     elif idx >= len(my_list):
