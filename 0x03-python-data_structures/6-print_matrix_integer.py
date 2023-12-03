4631b6e9c9d06e8d8bd9c670670ee7ebede2576f
suruur <surrarweesh@gmail.com>
2023-12-03 10:32:41 +0000

    ADDING

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
-    if __name__ == "__main__":
-        if matrix == [[]]:
-            print("$")
-        else:
-            for row in matrix:
-                for element in row:
-                    print("{:d}".format(element), end=" " if element != row[-1] else "$")
-                print()
+    if matrix == [[]]:
+        print("$")
+    else:
+        for row in matrix:
+            for element in row:
+                print("{:d}".format(element), end=" " if element != row[-1] else "$")
+            print()
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if __name__ == "__main__":
-        a = tuple_a + (0, 0)
-        b = tuple_b + (0, 0)
-        res = (a[0] + b[0], a[1] + b[1])
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    res = (a[0] + b[0], a[1] + b[1])
 
-        return res
+    return res
