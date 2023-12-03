3151e896a11dafc73b5f50213ef304ac7d036e4e
suruur <surrarweesh@gmail.com>
2023-12-03 09:41:06 +0000

    adding validation

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
 
-    if __name__ == "__main__":
+    if len(my_list) == 0:
+        return 0
+    else:
         idx = -1
         for i in my_list:
             print("{}".format(my_list[idx]))
