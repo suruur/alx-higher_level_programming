3339df247bc5475315bb347307c210b1f7600239
suruur <surrarweesh@gmail.com>
2023-12-03 10:26:33 +0000

    ASDSD

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
 
-    if __name__ == "__main__":
-        res = ""
-        for char in my_string:
-            if char.lower() != 'c' or char.upper() != 'C':
-                res += char
+    res = ""
+    for char in my_string:
+        if char.lower() != 'c' or char.upper() != 'C':
+            res += char
 
-        return (res)
+    return (res)
