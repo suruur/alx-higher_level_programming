b0c21386b0bebe7e59bf44799b1d1c40745c8c9e
suruur <surrarweesh@gmail.com>
2023-12-03 12:00:07 +0000

    One element

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,6 +6,8 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
+    if len(my_list) == 1:
+        return new_l=[]
     new_l = my_list[:idx] + my_list[idx + 1:]
 
     return new_l
