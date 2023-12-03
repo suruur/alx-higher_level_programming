04c831da6fdd3a11013d58880b03b421fa0890e8
suruur <surrarweesh@gmail.com>
2023-12-03 09:31:50 +0000

    Without importing

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
 
-    if __name__ == "__main__":
-        if len(my_list) == 0:
-            print()
-        else:
-            for i in my_list:
-                print("{:d}".format(i))
+    if len(my_list) == 0:
+        print()
+    else:
+        for i in my_list:
+            print("{:d}".format(i))
