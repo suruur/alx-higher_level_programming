b62a1a4f205f2431f011fa32ae68a39bc4d9148f
suruur <surrarweesh@gmail.com>
2023-12-03 07:19:13 +0000

    A

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+
+    if __name__ == "__main__":
+        for i in my_list:
+            print("{:d}".format(i))
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+
+    if __name__ == "__main__":
+        if not my_list:
+            return None
+
+        max_v = my_list[0]
+        for num in my_list:
+            if num > max_v:
+                max_v = num
+
+        return max_v
