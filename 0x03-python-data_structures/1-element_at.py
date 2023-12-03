41de7fd70f3b677042215e2f646268a89ef0e82d
suruur <surrarweesh@gmail.com>
2023-12-03 09:36:32 +0000

    SD

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
 
-    if __name__ == "__main__":
-        if idx < 0:
-            return None
+    if idx < 0:
+        return None
 
-        if idx >= len(my_list):
-            return None
+    if idx >= len(my_list):
+        return None
 
-        return (my_list[idx])
+    return (my_list[idx])
