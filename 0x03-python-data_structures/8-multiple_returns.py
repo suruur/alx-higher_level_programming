97144371ebbce664f12e15a5b9d01a3f7326c5cc
suruur <surrarweesh@gmail.com>
2023-12-03 07:14:04 +0000

    d

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+
+    if __name__ == "__main__":
+        length = len(sentence)
+        f_c = sentence[0] if length > 0 else None
+        return (length, f_c)
