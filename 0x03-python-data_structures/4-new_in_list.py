158500cad7be69c05fd20b371199c8ffaa076eaf
suruur <surrarweesh@gmail.com>
2023-12-03 10:00:51 +0000

    Replace

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
-    if __name__ == "__main__":
-        copied_list = my_list
-        if idx < 0:
-            return (copied_list)
-        elif idx > len(my_list):
-            return (copied_list)
-        else:
-            copied_list[idx] = element
-            return (copied_list)
+    copied_list = my_list
+    if idx < 0:
+        return (copied_list)
+    elif idx >= len(my_list):
+        return (copied_list)
+    else:
+        copied_list[idx] = element
+        return (copied_list)
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -3,4 +3,4 @@ print("Inside Main...")
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
 my_list = [1]
-print_reversed_list_integer(my_list)
+print_reversed_list_integer()
