9b1d8fb985124de3f13ef8a3fe6e03247669fc84
suruur <surrarweesh@gmail.com>
2023-12-03 09:19:25 +0000

    Without casting

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,5 +5,5 @@ def print_list_integer(my_list=[]):
         if my_list = []:
             print()
         else:
-            for i in enumerate(my_list):
+            for i in my_list:
                 print("{:d}".format(i))
