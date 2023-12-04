2903e19c7c8798ea230248353051f470b70306aa
suruur <surrarweesh@gmail.com>
2023-12-04 09:18:22 +0000

    ADDING MATRIX

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
 
-    if len(matrix) == 0:
+    if matrix == [[]]:
         print("$")
     else:
         for row in matrix:
             for element in row:
                 print("{:d}".format(element), end=" " if element != row[-1] else "$")
             print()
+#    if matrix:
+#        for row in matrix:
+#            for elem in row:
+#                print("{}".format(elem, end=" " if elem != row[-1] else "$"))
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 print("Inside Main ...")
-new_in_list = __import__('4-new_in_list').new_in_list
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
-my_list = [0,1,3,4,5,6]
-idx = 3
-ele = 9
-n_list = new_in_list(my_list, idx, ele)
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+ ]
+#idx = 3
+#ele = 9
+#n_list = new_in_list(my_list, idx, ele)
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
 
-
-print(n_list)
-print(my_list)
+#print(n_list)
+#print(my_list)
