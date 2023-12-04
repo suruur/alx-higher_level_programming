175920141b51d9d1e7ec459e6097424ad4d6fc9f
suruur <surrarweesh@gmail.com>
2023-12-04 09:27:30 +0000

    DELEL

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,15 +5,15 @@ def delete_at(my_list=[], idx=0):
         return 0
     if idx < 0 or idx >= len(my_list):
         return my_list
-#    if len(my_list) != 1:
-#        my_list = my_list[:idx] + my_list[idx + 1:]
-#    else:
-#        my_list = []
+    if len(my_list) != 1:
+        my_list = my_list[:idx] + my_list[idx + 1:]
+    else:
+       my_list = []
 
-    del_list = []
-    for i in range(0, len(my_list), 1):
-        del_list[i] = my_list[i] if i != idx else continue
+#    del_list = []
+#    for i in range(0, len(my_list), 1):
+#        del_list[i] = my_list[i] if i != idx else continue
 
-    my_list = del_list
+ #   my_list = del_list
 
     return my_list
