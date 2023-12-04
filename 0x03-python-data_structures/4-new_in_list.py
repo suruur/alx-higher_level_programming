57e8e25d5f3d0183db9f12511064e8e2d5e234cf
suruur <surrarweesh@gmail.com>
2023-12-04 08:16:41 +0000

    ADDING NEW ELE AT END

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
+    new_list = my_list
     if idx < 0:
-        return my_list.copy()
+        return new_list
     elif idx >= len(my_list):
-        return my_list.copy()
+        return new_list
+    elif len(my_list) == 1:
+        new_list[idx] = element
     else:
         new_list = my_list[:idx] + [element] + [idx + 1:]
 
