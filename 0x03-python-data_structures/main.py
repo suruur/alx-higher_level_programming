8e7571a2b167ac062e31c97d628cc807f7532bc5
suruur <surrarweesh@gmail.com>
2023-12-04 08:27:36 +0000

    Wiht copy

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 print("Inside Main ...")
-divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
+new_in_list = __import__('4-new_in_list').new_in_list
 
 my_list = [0,1,3,4,5,6]
-list_res = divisible_by_2(my_list)
+idx = 3
+ele = 9
+n_list = new_in_list(my_list, idx, ele)
 
 
-i = 0
-while i < len(list_res):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_res[i] else "is not"))
-    i += 1
+print(n_list)
+print(my_list)
