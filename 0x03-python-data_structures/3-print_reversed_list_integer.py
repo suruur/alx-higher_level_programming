062e3c05bc917fb3ab2a9de58020eca986b9851a
suruur <surrarweesh@gmail.com>
2023-12-04 08:36:33 +0000

    removing the d format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,4 +7,4 @@ def print_reversed_list_integer(my_list=[]):
         print("{:d}".format(my_list[0]))
     else:
         for i in range(len(my_list) - 1, -1, -1):
-            print("{:d}".format(my_list[i]))
+            print("{}".format(my_list[i]))
