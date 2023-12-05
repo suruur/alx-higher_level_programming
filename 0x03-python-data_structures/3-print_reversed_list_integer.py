bd783f455d1ff09a233eb13e6b30665c71bf93b9
suruur <surrarweesh@gmail.com>
2023-12-05 13:22:10 +0000

    NONE

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
 
+    if my_list == None:
+        return 0
     if len(my_list) == 0:
         return 0
     elif len(my_list) == 1:
