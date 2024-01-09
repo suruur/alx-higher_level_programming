abff39358194f12f4a95298099db751307f42785
suruur <surrarweesh@gmail.com>
2024-01-09 04:38:07 +0000

    5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def from_json_string(my_str):
-    if my_str.startswith('"') and my_str.endswith('"'):
-        return my_str[1:-1].replace('\\"', '"')
-    else:
-        return None
+def save_to_json_file(my_obj, filename):
+    json_str = '"' + str(my_obj).replace('"', '\\"') + '"'
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(json_str)
