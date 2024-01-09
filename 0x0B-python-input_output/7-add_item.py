cac0b6a46d127c0b8a1580aedbfbf556e2291cab
suruur <surrarweesh@gmail.com>
2024-01-09 21:16:46 +0000

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,11 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
+import 6-load_from_json_file as ld
+import 5-save_to_json_file as sv
+import sys
+if __name__ == "__main__":
+    args = sys.argv[1]
+    items = ld.load_from_json_file("add_item.json")
+    items_to_sv = items + args
+    sv.save_to_json_file(items_to_sv, "add_item.json")
+    ld_cn = ld.load_from_json_file("add_item.json")
+    print(ld_cn)
