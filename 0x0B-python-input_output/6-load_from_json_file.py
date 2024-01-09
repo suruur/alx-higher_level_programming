940008933dceced559e5e26be69c0e3aa97c68a3
suruur <surrarweesh@gmail.com>
2024-01-09 04:44:16 +0000

    6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,8 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
+def load_from_json_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        json_str = file.read().strip()
+    if json_str.startswith('"') and json_str.endswith('"'):
+        return json_str[1:-1].replace('\\"', '"')
+    else:
+        return None
